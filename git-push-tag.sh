3715ae05c10e6a5aae969c1249c5d9db01d8e84b
Rafael Teixeira <rafael.teixeira@gorilainvest.com.br>
2021-04-02 11:01:45 -0300

    initial commit

---
+++ git-push-tag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# sudo chmod 777 git-push-tag.sh 
+# ./git-push-tag.sh v0.6
+
+echo "GIT pushing $1"
+
+git add . && git commit --amend --no-edit
+git tag $1 && git push origin $1
+
+# delete tag:
+# git push origin :refs/tags/v1.0  
